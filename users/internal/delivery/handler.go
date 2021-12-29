15082673ef35dc3a867493bc8e2a4da27a5b4247
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2021-12-29 16:19:06 +0300

    [ users ] JWT auth

--- users/internal/delivery/handler.go
+++ users/internal/delivery/handler.go
@@ -4,18 +4,23 @@ import (
 	"context"
 	"github.com/KirillMironov/rapu/domain"
 	"github.com/KirillMironov/rapu/internal/delivery/proto"
+	"github.com/KirillMironov/rapu/pkg/auth"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Handler struct {
-	service domain.UsersService
+	service      domain.UsersService
+	tokenManager auth.TokenManager
 	proto.UnimplementedUsersServer
 }
 
-func NewHandler(usersService domain.UsersService) *grpc.Server {
+func NewHandler(usersService domain.UsersService, tokenManager auth.TokenManager) *grpc.Server {
 	var server = grpc.NewServer()
 	proto.RegisterUsersServer(server, &Handler{
-		service: usersService,
+		service:      usersService,
+		tokenManager: tokenManager,
 	})
 	return server
 }
@@ -47,4 +52,13 @@ func (h *Handler) SignIn(ctx context.Context, request *proto.SignInRequest) (*pr
 	}
 
 	return &proto.Response{AccessToken: token}, nil
+}
+
+func (h *Handler) Authenticate(ctx context.Context, request *proto.AuthRequest) (*proto.AuthResponse, error) {
+	userId, err := h.tokenManager.VerifyAuthToken(request.AccessToken)
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, err.Error())
+	}
+
+	return &proto.AuthResponse{UserId: userId}, nil
 }
