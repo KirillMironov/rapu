3b27d8558dec78fa6a9b72f8b8df1eb8f4175bbe
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2022-01-03 15:18:05 +0300

    [ users ] Handler test

--- users/internal/delivery/handler_test.go
+++ users/internal/delivery/handler_test.go
@@ -2,7 +2,6 @@ package delivery
 
 import (
 	"context"
-	"github.com/KirillMironov/rapu/users/domain"
 	"github.com/KirillMironov/rapu/users/internal/delivery/proto"
 	"github.com/KirillMironov/rapu/users/test/mocks"
 	"github.com/stretchr/testify/assert"
@@ -17,62 +16,68 @@ import (
 )
 
 const (
-	bufSize  = 1024 * 1024
 	email    = "lisa@gmail.com"
 	password = "qwerty"
 	username = "Lisa"
 )
 
-var (
-	lis *bufconn.Listener
-	ctx = context.Background()
-)
+var ctx = context.Background()
 
-func init() {
-	lis = bufconn.Listen(bufSize)
+func newClient() (proto.UsersClient, func()) {
+	var listener = bufconn.Listen(1024 * 1024)
+	var server = grpc.NewServer()
 
-	s := grpc.NewServer()
-	proto.RegisterUsersServer(s, &Handler{
+	proto.RegisterUsersServer(server, &Handler{
 		service: mocks.UsersServiceMock{},
 		logger:  mocks.LoggerMock{},
 	})
 
 	go func() {
-		log.Fatal(s.Serve(lis))
+		log.Fatal(server.Serve(listener))
 	}()
-}
 
-func bufDialer(context.Context, string) (net.Conn, error) {
-	return lis.Dial()
+	conn, err := grpc.DialContext(ctx, "", grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(func(context.Context, string) (net.Conn, error) {
+			return listener.Dial()
+		}))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return proto.NewUsersClient(conn), func() {
+		conn.Close()
+	}
 }
 
 func TestHandler_SignUp(t *testing.T) {
 	t.Parallel()
 
 	testCases := []struct {
-		user               domain.User
+		username           string
+		email              string
+		password           string
 		expectedStatusCode codes.Code
 	}{
-		{domain.User{Username: username, Email: email, Password: password}, codes.OK},
-		{domain.User{}, codes.InvalidArgument},
+		{username, email, password, codes.OK},
+		{"", email, password, codes.InvalidArgument},
+		{username, "", password, codes.InvalidArgument},
+		{username, email, "", codes.InvalidArgument},
+		{"", "", password, codes.InvalidArgument},
+		{"", email, "", codes.InvalidArgument},
+		{username, "", "", codes.InvalidArgument},
+		{"", "", "", codes.InvalidArgument},
 	}
 
-	conn, err := grpc.DialContext(ctx, "bufnet", grpc.WithContextDialer(bufDialer),
-		grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer conn.Close()
-
-	client := proto.NewUsersClient(conn)
+	client, closeConn := newClient()
+	defer closeConn()
 
 	for _, tc := range testCases {
 		tc := tc
 
 		resp, err := client.SignUp(ctx, &proto.SignUpRequest{
-			Username: tc.user.Username,
-			Email:    tc.user.Email,
-			Password: tc.user.Password,
+			Username: tc.username,
+			Email:    tc.email,
+			Password: tc.password,
 		})
 
 		e, _ := status.FromError(err)
@@ -82,6 +87,72 @@ func TestHandler_SignUp(t *testing.T) {
 			assert.NotEmpty(t, resp.GetAccessToken())
 		} else {
 			assert.Empty(t, resp.GetAccessToken())
+		}
+	}
+}
+
+func TestHandler_SignIn(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		email              string
+		password           string
+		expectedStatusCode codes.Code
+	}{
+		{email, password, codes.OK},
+		{"", password, codes.InvalidArgument},
+		{email, "", codes.InvalidArgument},
+		{"", "", codes.InvalidArgument},
+	}
+
+	client, closeConn := newClient()
+	defer closeConn()
+
+	for _, tc := range testCases {
+		tc := tc
+
+		resp, err := client.SignIn(ctx, &proto.SignInRequest{
+			Email:    tc.email,
+			Password: tc.password,
+		})
+
+		e, _ := status.FromError(err)
+		assert.Equal(t, tc.expectedStatusCode, e.Code())
+
+		if e.Code() == codes.OK {
+			assert.NotEmpty(t, resp.GetAccessToken())
+		} else {
+			assert.Empty(t, resp.GetAccessToken())
+		}
+	}
+}
+
+func TestHandler_Authenticate(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		token              string
+		expectedStatusCode codes.Code
+	}{
+		{"token", codes.OK},
+		{"", codes.InvalidArgument},
+	}
+
+	client, closeConn := newClient()
+	defer closeConn()
+
+	for _, tc := range testCases {
+		tc := tc
+
+		resp, err := client.Authenticate(ctx, &proto.AuthRequest{AccessToken: tc.token})
+
+		e, _ := status.FromError(err)
+		assert.Equal(t, tc.expectedStatusCode, e.Code())
+
+		if e.Code() == codes.OK {
+			assert.NotEmpty(t, resp.GetUserId())
+		} else {
+			assert.Empty(t, resp.GetUserId())
 		}
 	}
 }
