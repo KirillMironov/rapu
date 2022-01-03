14657a00a7031e7a87f3e2620077c0272dfaa4d1
Kirill Mironov <k.mironov01@gmail.com>
2022-01-03 23:30:21 +0300

    [ users ] Users service tests

--- users/internal/service/users.go
+++ users/internal/service/users.go
@@ -1,10 +1,13 @@
 package service
 
 import (
+	"errors"
 	"github.com/KirillMironov/rapu/users/domain"
 	"github.com/KirillMironov/rapu/users/pkg/auth"
 	"golang.org/x/crypto/bcrypt"
 )
+
+var errNotEnoughArgs = errors.New("not enough arguments")
 
 type UsersService struct {
 	repository   domain.UsersRepository
@@ -19,11 +22,14 @@ func NewUsersService(repository domain.UsersRepository, tokenManager auth.TokenM
 }
 
 func (u *UsersService) SignUp(user domain.User) (string, error) {
+	if user.Username == "" || user.Email == "" || user.Password == "" {
+		return "", errNotEnoughArgs
+	}
+
 	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
 	if err != nil {
 		return "", err
 	}
-
 	user.Password = string(hash)
 
 	userId, err := u.repository.Create(user)
@@ -35,6 +41,10 @@ func (u *UsersService) SignUp(user domain.User) (string, error) {
 }
 
 func (u *UsersService) SignIn(input domain.User) (string, error) {
+	if input.Email == "" || input.Password == "" {
+		return "", errNotEnoughArgs
+	}
+
 	user, err := u.repository.GetByEmail(input.Email)
 	if err != nil {
 		return "", err
@@ -49,5 +59,9 @@ func (u *UsersService) SignIn(input domain.User) (string, error) {
 }
 
 func (u *UsersService) Authenticate(token string) (string, error) {
+	if token == "" {
+		return "", errNotEnoughArgs
+	}
+
 	return u.tokenManager.VerifyAuthToken(token)
 }
