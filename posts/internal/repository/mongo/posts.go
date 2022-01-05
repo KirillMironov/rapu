cd101f3d78aad827f9bfaef25c0476a425122ee3
Kirill Mironov <k.mironov01@gmail.com>
2022-01-05 20:55:13 +0300

    [ posts ] Integration test

--- posts/internal/repository/mongo/posts.go
+++ posts/internal/repository/mongo/posts.go
@@ -1,11 +1,15 @@
 package mongo
 
 import (
+	"context"
 	"github.com/KirillMironov/rapu/posts/domain"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+var ctx = context.Background()
 
 type PostsRepository struct {
 	db *mongo.Collection
@@ -16,25 +20,27 @@ func NewPostsRepository(db *mongo.Collection) *PostsRepository {
 }
 
 func (p *PostsRepository) Create(post domain.Post) (string, error) {
-	res, err := p.db.InsertOne(nil, post)
+	post.Id = primitive.NewObjectID()
+
+	res, err := p.db.InsertOne(ctx, post)
 	if err != nil {
 		return "", err
 	}
 
-	return res.InsertedID.(string), nil
+	return res.InsertedID.(primitive.ObjectID).Hex(), nil
 }
 
 func (p *PostsRepository) GetByUserId(userId string) ([]domain.Post, error) {
 	opts := options.Find()
-	opts.SetSort(bson.D{{"created_at", -1}})
+	opts.SetSort(bson.D{{Key: "created_at", Value: -1}})
 
-	cur, err := p.db.Find(nil, bson.M{"user_id": userId}, opts)
+	cur, err := p.db.Find(ctx, bson.M{"user_id": userId}, opts)
 	if err != nil {
 		return nil, err
 	}
 
 	var posts []domain.Post
-	err = cur.All(nil, &posts)
+	err = cur.All(ctx, &posts)
 	if err != nil {
 		return nil, err
 	}
---
+++ posts/test/integration/posts_test.go
@@ -0,0 +1,129 @@
+//go:build integration
+
+package integration
+
+import (
+	"context"
+	"fmt"
+	"github.com/KirillMironov/rapu/posts/domain"
+	_mongo "github.com/KirillMironov/rapu/posts/internal/repository/mongo"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"testing"
+	"time"
+)
+
+const (
+	mongoPort  = "27017"
+	database   = "rapu"
+	collection = "posts"
+	userId     = "1"
+)
+
+var (
+	ctx  = context.Background()
+	post = domain.Post{
+		UserId:    userId,
+		Message:   "Hello",
+		CreatedAt: time.Now(),
+	}
+)
+
+func TestPosts_Create(t *testing.T) {
+	db, terminate, err := mongoSetup()
+	defer terminate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	repo := _mongo.NewPostsRepository(db)
+
+	id, err := repo.Create(post)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id)
+
+	id, err = repo.Create(post)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id)
+
+	id, err = repo.Create(domain.Post{
+		UserId:    "2",
+		Message:   "Empty",
+		CreatedAt: time.Now(),
+	})
+	assert.NoError(t, err)
+	assert.NotEmpty(t, id)
+
+	posts, err := repo.GetByUserId(userId)
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(posts))
+	assert.NotEmpty(t, posts[0])
+	assert.NotEmpty(t, posts[1])
+}
+
+func TestPosts_GetByUserId(t *testing.T) {
+	db, terminate, err := mongoSetup()
+	defer terminate()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	repo := _mongo.NewPostsRepository(db)
+
+	_, err = repo.Create(post)
+	assert.NoError(t, err)
+
+	_, err = repo.Create(domain.Post{
+		UserId:    userId,
+		Message:   "New post",
+		CreatedAt: time.Now().Add(time.Hour),
+	})
+	assert.NoError(t, err)
+
+	posts, err := repo.GetByUserId(userId)
+	assert.NoError(t, err)
+	assert.True(t, posts[0].CreatedAt.After(posts[1].CreatedAt))
+}
+
+func mongoSetup() (*mongo.Collection, func(), error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "mongo:4.4-rc-focal",
+		ExposedPorts: []string{mongoPort},
+	}
+
+	container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	ip, err := container.Host(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	mappedPort, err := container.MappedPort(ctx, mongoPort)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	conn := fmt.Sprintf("mongodb://%s:%s", ip, mappedPort.Port())
+
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(conn))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return client.Database(database).Collection(collection), func() {
+		container.Terminate(ctx)
+	}, nil
+}
