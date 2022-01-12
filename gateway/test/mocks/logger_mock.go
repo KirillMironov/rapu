a22800d7fe5d5f7810b491e87298c48ab83d5046
Kirill Mironov <k.mironov01@gmail.com>
2022-01-12 19:23:13 +0300

    [ gateway ] Remove unused mocks parameters

--- gateway/test/mocks/logger_mock.go
+++ gateway/test/mocks/logger_mock.go
@@ -2,10 +2,6 @@ package mocks
 
 type LoggerMock struct{}
 
-func (LoggerMock) Info(args ...interface{}) {}
+func (LoggerMock) Info(...interface{}) {}
 
-func (LoggerMock) Infof(template string, args ...interface{}) {}
-
-func (LoggerMock) Error(args ...interface{}) {}
-
-func (LoggerMock) Errorf(template string, args ...interface{}) {}
+func (LoggerMock) Error(...interface{}) {}
