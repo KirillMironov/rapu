8bc9797e2d1f866188ecae7d316e49a46222b03d
Kirill Mironov <k.mironov01@gmail.com>
2022-01-12 14:46:36 +0300

    [ users ] Move TokenManager interface

--- users/test/mocks/token_manager_mock.go
+++ users/test/mocks/token_manager_mock.go
@@ -2,10 +2,10 @@ package mocks
 
 type TokenManagerMock struct{}
 
-func (TokenManagerMock) GenerateAuthToken(userId string) (string, error) {
+func (TokenManagerMock) Generate(userId string) (string, error) {
 	return "token", nil
 }
 
-func (TokenManagerMock) VerifyAuthToken(token string) (string, error) {
+func (TokenManagerMock) Verify(token string) (string, error) {
 	return "token", nil
 }
