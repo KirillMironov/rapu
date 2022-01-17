2b74e3651eb964b335c7df9b076438db202df858
Kirill Mironov <k.mironov01@gmail.com>
2022-01-17 13:46:35 +0300

    [ messenger ] Static

---
+++ messenger/static/main.js
@@ -0,0 +1,45 @@
+let socket;
+
+function connect() {
+    if (socket != null) {
+        socket.close();
+        document.getElementById('messages_area').value = '';
+    }
+
+    socket = new WebSocket('ws://localhost:7004/connect' +
+        '?userId='
+        + document.getElementById('userId').value
+        + '&toUserId='
+        + document.getElementById('toUserId').value);
+
+    console.log('Attempting Connection...');
+
+    socket.onopen = () => {
+        console.log('Successfully Connected');
+    };
+
+    socket.onmessage = message => {
+        let json = JSON.parse(message.data);
+        console.log(json)
+        document.getElementById('messages_area').value += `${json.from}: ${String(json.text)}\r\n`;
+    };
+
+    socket.onclose = event => {
+        console.log('Socket Closed Connection: ', event);
+    };
+
+    socket.onerror = error => {
+        console.log('Socket Error: ', error);
+    };
+}
+
+function sendMessage() {
+    let message = document.getElementById('message_input').value.toString();
+
+    if (socket == null || message === '') {
+        return;
+    }
+
+    socket.send(message);
+    document.getElementById('message_input').value = '';
+}
