dcaa912f7dbf0364d39d8822523824bcfcd1455a
Kirill Mironov <k.mironov01@gmail.com>
2022-06-07 23:31:19 +0300

    Graceful Shutdown

--- gateway/main.go
+++ gateway/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"github.com/KirillMironov/rapu/gateway/config"
 	"github.com/KirillMironov/rapu/gateway/internal/delivery"
 	"github.com/KirillMironov/rapu/gateway/internal/delivery/proto"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
@@ -44,6 +50,32 @@ func main() {
 
 	// App
 	handler := delivery.NewHandler(usersClient, postsClient, logger)
+
+	// Gin
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: handler.InitRoutes(),
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Fatal(err)
+		}
+	}()
+
 	logger.Infof("gateway started on port %s", cfg.Port)
-	logger.Fatal(handler.InitRoutes().Run(":" + cfg.Port))
+
+	// Graceful Shutdown
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	<-quit
+	logger.Info("shutting down http server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Fatal(err)
+	}
 }
